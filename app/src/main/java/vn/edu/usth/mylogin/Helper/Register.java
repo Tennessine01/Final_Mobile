0f6df42a77a61b4fb69d7b7350e9ddfb007d06c6
TGraceAn <annt.bi12-008@st.usth.edu.vn>
2023-10-07 04:51:28 +0700

    added some account behaviours

--- app/src/main/java/vn/edu/usth/mylogin/Helper/Register.java
+++ app/src/main/java/vn/edu/usth/mylogin/Helper/Register.java
@@ -14,24 +14,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import vn.edu.usth.mylogin.R;
 
 public class Register extends AppCompatActivity {
 
     CheckBox checkbox;
-    TextView emailRegisText, passwordRegisText, passwordConfirmRegisText;
+    TextView usernameRegis, emailRegisText, passwordRegisText, passwordConfirmRegisText;
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
     Button buttonReg;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        usernameRegis = findViewById(R.id.Username);
+
         emailRegisText = findViewById(R.id.Register_Email);
         passwordRegisText = findViewById(R.id.Register_Password);
         passwordConfirmRegisText = findViewById(R.id.Register_Confirm_Password);
@@ -40,13 +52,20 @@ public class Register extends AppCompatActivity {
         buttonReg.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String email, password, con_password;
+                String username, email, password, con_password;
                 CheckBox check;
+
+                username = usernameRegis.getText().toString();
+
                 email = emailRegisText.getText().toString();
                 password = passwordRegisText.getText().toString();
                 con_password = passwordConfirmRegisText.getText().toString();
                 check = checkbox;
 
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(Register.this, "Enter username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
                     return;
@@ -72,6 +91,19 @@ public class Register extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(Register.this, "Account created.",
                                             Toast.LENGTH_SHORT).show();
+                                    userID = mAuth.getCurrentUser().getUid();
+                                    DocumentReference documentReference = fStore.collection("users").document(userID);
+                                    Map<String,Object> user = new HashMap<>();
+                                    user.put("uName", username);
+                                    user.put("email", email);
+                                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Toast.makeText(Register.this, "Saved info.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
                                     Intent intent = new Intent(getApplicationContext(), Login.class);
                                     startActivity(intent);
                                     finish();
